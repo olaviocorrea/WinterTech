8a1723c1c9c16d25ebfc33e9e6836be972dd2e6d
Nayara <nayarargs10@gmail.com>
2023-06-03 17:45:51 -0400

    atualizando javaScript

--- JSProdutos.js
+++ JSProdutos.js
@@ -1,35 +1,30 @@
-const arrayProdutos = [
-    {
-        Img:"",
-        descrição:"Conjunto de Camisa",
-        valor:"R$ 199,00",
-        botão:"Adicionar ao carrinho"
-    },
-    {
-        Img:"",
-        descrição:"Legging",
-        valor:"R$ 99,00",
-        botão:"Adicionar ao carrinho"
-    },
-    {
-        Img:"",
-        descrição:"Casaco ",
-        valor:"R$ 139,00",
-        botão:"Adicionar ao carrinho"
-    },
-]
+var carrinho = [];
+    
+    function adicionarAoCarrinho(nome, preco) {
+      carrinho.push({ nome: nome, preco: preco });
+      atualizarCarrinho();
+    }
+    
+    function atualizarCarrinho() {
+      var container = document.getElementById("itens-carrinho");
+      container.innerHTML = "";
+      
+      var total = 0;
+      
+      for (var i = 0; i < carrinho.length; i++) {
+        var produto = carrinho[i];
+        
+        var divProduto = document.createElement("div");
+        divProduto.classList.add("produto-carrinho");
+        divProduto.innerHTML = "<img src='caminho/para/imagem" + (i+1) + ".jpg' alt='" + produto.nome + "'><p>" + produto.nome + "</p><p>R$" + produto.preco.toFixed(2) + "</p>";
+        
+        container.appendChild(divProduto);
+        
+        total += produto.preco;
+      }
+      
+      var totalCarrinho = document.getElementById("total-carrinho");
+      totalCarrinho.innerHTML = "Total: R$ " + total.toFixed(2);
+    }
 
-for(let i=0; i<arrayProdutos.length;i++) {
-    const product=document.createElement("product");
-    product.className =`product -${i+1}`
-    console.log(product);
 
-    product.innerHTML =`
-    <img>${arrayProdutos[i].Img}</img>
-    <h3>${arrayProdutos[i].descrição}</h3>
-    <p>${arrayProdutos[i].valor}</p>
-    <button>${arrayProdutos[i].botão}</button>`
-
-    const section= document.getElementsByClassName("featured-products")[0]
-    section.appendChild(product)
-}
